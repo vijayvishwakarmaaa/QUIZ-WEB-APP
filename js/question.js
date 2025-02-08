dd5d1d84a85f09bd8b031fdb42a1d22b09346cab
vijayvishwakarmaaa <vijayvish078@gmail.com>
2025-02-08 21:24:35 +0530

    First Commit

---
+++ js/question.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options , and answer
+let question = [
+    {
+        numb: 1,
+        question: "What is HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Markup Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "",
+        options: [
+            "Cascading Style Sheet",
+            "Colurful Style Sheet",
+            "Common Style Sheet",
+            "Computer Styel Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hypertext Programing",
+            "Hypertext Preprograming",
+            "Hometext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stands for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXtra Multi-Progaming Language",
+            "eXamine Multiple Language"
+        ]
+    },
+]
